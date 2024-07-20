118ff476ad69073471aa6624a2ef519da8004a75
Nitin <nitinsingh919293@gmail.com>
2024-07-20 13:15:24 +0530

    Backup Till Reading and writing

---
+++ Backup-Till-Reading-Writing(SummaryCheck)/pte/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./Components/Nav/Nav";
+import QueData from "./Components/QueData";
+import Reading from "./Components/Reading/Reading";
+import Listening from "./Components/Listening";
+import Speaking from "./Components/Speaking";
+import Writing from "./Components/Writing/Writing";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Reading />} />
+        <Route path="/reading" element={<Reading />} />
+        <Route path="/writing" element={<Writing />} />
+        <Route path="/listening" element={<Listening />} />
+        <Route path="/speaking" element={<Speaking />} />
+        {/* <Route path="/dragDrop" element={<DragAndDrop />} /> */}
+
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Backup-Till-Reading-Writing(SummaryCheck)/pte/src/Components/Speaking.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Speaking() {
+  return (
+    <div className="main-Div">
+    <div className="main-Div-Content">
+      <div className="speaking-main">
+        <div className="speaking-main-content">
+        Speaking 
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Speaking
